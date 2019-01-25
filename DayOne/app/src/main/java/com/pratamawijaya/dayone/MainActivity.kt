6b07ed891f9cee7c308386f449bcce439dd7d07f
pratamawijaya <set.mnemonix@gmail.com>
2019-01-25 20:58:12 +0700

    init project day one

---
+++ DayOne/app/src/main/java/com/pratamawijaya/dayone/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.pratamawijaya.dayone
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+        tvHelloWorld.text = "Dilo Jogja"
+    }
+}
